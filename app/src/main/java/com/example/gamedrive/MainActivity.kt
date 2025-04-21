ecf1df566bd92222ef5b2410783ebd82a603d8f0
LukaLominadze <fguru74@gmail.ccom>
2025-04-21 14:38:32 +0400

    adding search

--- app/src/main/java/com/example/gamedrive/MainActivity.kt
+++ app/src/main/java/com/example/gamedrive/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.gamedrive
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -22,6 +25,9 @@ import retrofit2.HttpException
 class MainActivity : AppCompatActivity(), RecyclerViewInterface {
     private lateinit var gameCardModels: ArrayList<GameCardModel>
     private lateinit var gameArray: ArrayList<Game>
+    private lateinit var searchEditText: EditText
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerViewAdapter: GameCardRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +40,7 @@ class MainActivity : AppCompatActivity(), RecyclerViewInterface {
         }
 
         gameCardModels = ArrayList<GameCardModel>()
+        searchEditText = findViewById<EditText>(R.id.searchEditText)
         RestClient.init()
 
         lifecycleScope.launch {
@@ -47,8 +54,8 @@ class MainActivity : AppCompatActivity(), RecyclerViewInterface {
                     Toast.makeText(this@MainActivity, "Internet is required", Toast.LENGTH_SHORT).show()
                 }
 
-                val recyclerView = findViewById<RecyclerView>(R.id.NoteRecyclerView)
-                val recyclerViewAdapter = GameCardRecyclerViewAdapter(
+                recyclerView = findViewById<RecyclerView>(R.id.NoteRecyclerView)
+                recyclerViewAdapter = GameCardRecyclerViewAdapter(
                     this@MainActivity,
                     this@MainActivity,
                     gameCardModels,
@@ -56,6 +63,8 @@ class MainActivity : AppCompatActivity(), RecyclerViewInterface {
                 )
                 recyclerView.adapter = recyclerViewAdapter
                 recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+
+                setOnSearchTextChanged()
             } catch (e: IOException) {
                 Toast.makeText(this@MainActivity, "Invalid request", Toast.LENGTH_SHORT).show()
             } catch (e: HttpException) {
@@ -86,6 +95,34 @@ class MainActivity : AppCompatActivity(), RecyclerViewInterface {
             }
             gameCardModels.add(GameCardModel(game.name, categories, game.image))
         }
+    }
+
+    private fun setOnSearchTextChanged() {
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val query = s.toString()
+                recyclerViewAdapter = GameCardRecyclerViewAdapter(
+                    this@MainActivity,
+                    this@MainActivity,
+                    gameCardModels.filter { it.title.contains(query, ignoreCase = true) }.toCollection(
+                        ArrayList()),
+                    gameArray.filter { it.name!!.contains(query, ignoreCase = true) }.toCollection(
+                        ArrayList())
+                )
+                recyclerView.adapter = recyclerViewAdapter
+                recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                p0: CharSequence?, p1: Int, p2: Int, p3: Int
+            ) {
+            }
+
+        })
     }
 
     override fun onClick(game: Game)  {
